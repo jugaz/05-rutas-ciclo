a2aba70248ecb9475c67f8ba7f901231df155edf
Jesús Antonio Ugaz Vegas <jesusugazv@gmail.com>
2022-05-05 18:26:01 -0500

    Update

--- src/modules/shared/components/NavBar.vue
+++ src/modules/shared/components/NavBar.vue
@@ -1,14 +1,22 @@
 <template>
     
     <div>
-        <a href="/">Home</a>
-        <a href="/id">Brawl</a>
-        <a href="/about">About List</a>
+        <router-link to="/">Home</router-link>
+        <router-link to="/id">Brawl</router-link>
+        <router-link to="/about">About List</router-link>
 
     </div>
 
 </template>
 
 <style scoped>
+    div {
+        padding: 0 30px;
+    }
 
+    div a {
+        font-weight: bold;
+        color: #2c3e50;
+        margin: 0 10px;
+    }
 </style>
